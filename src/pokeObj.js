b8efd6102a1e508d73357b8fa717092918e7f3ff
liamH47 <liamhanafee@gmail.com>
2021-02-12 13:51:20 -0500

    added attribution for svgbackgrounds.com

--- src/pokeObj.js
+++ src/pokeObj.js
@@ -1,55 +1,46 @@
 export default [
     {
       id: 0,
-      position: 0,
       name: 'Bulbasaur',
       imageUrl: './img/bulbasaur.png'
     },
     {
       id: 1,
-      position: 1,
       name: 'Charmander',
       imageUrl: './img/charmander.png'
     },
     {
       id: 2,
-      position: 2,
       name: 'Squirtle',
       imageUrl: './img/squirtle.png'
     },
     {
       id: 3,
-      position: 3,
       name: 'Chikorita',
       imageUrl: './img/chikorita.png'
     },
     {
       id: 4,
-      position: 4,
       name: 'Cyndaquil',
       imageUrl: './img/cyndaquil.png'
     },
     {
       id: 5,
-      position: 5,
       name: 'Totodile',
       imageUrl: './img/totodile.png'
     },
     {
       id: 6,
-      position: 6,
       name: 'Treecko',
       imageUrl: './img/treecko.png'
     },
     {
       id: 7,
-      position: 7,
       name: 'Torchic',
       imageUrl: './img/torchic.png'
     },
     {
       id: 8,
-      position: 8,
       name: 'Mudkip',
       imageUrl: './img/mudkip.png'
     },
